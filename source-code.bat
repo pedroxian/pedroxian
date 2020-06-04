bcae0752ad14117000450ff24e5a0448ce65df77
pedroxian <44950875+pedroxian@users.noreply.github.com>
2020-06-04 22:39:35 +0200

    via batch
    essence loader

---
+++ source-code.bat
@@ -0,0 +1,40 @@
+PowerShell 7.0.1
+Copyright (c) Microsoft Corporation. All rights reserved.
+
+https://aka.ms/powershell
+Type 'help' to get help.
+
+PS C:\Users\Pedroxian> 'hentai --puffer.act -war/laos.defect rios cube--..moods --cigar.stiff --acrym
+>> topper.a frog.free --load submarine --clash.organizm -b.eta
+>> frost. setup ..common task:free.org ..load.tupper
+>> 'run
+ParserError:
+Line |
+   4 |  'run
+     |   ~~~
+     | Unexpected token 'run' in expression or statement.
+
+PS C:\Users\Pedroxian>
+PS C:\Users\Pedroxian> 'no over.stream acrym.preload --documentary _resolve:datacage
+>> tropper.tupy :made.in -taiwan --next.common _resto.raurante
+>> resco.code ---rise raiser.fs /.server..poch --leminite.luminate /6d.lights :war
+>> rigid
+>> 'run
+ParserError:
+Line |
+   5 |  'run
+     |   ~~~
+     | Unexpected token 'run' in expression or statement.
+
+PS C:\Users\Pedroxian> 'set sun.grow paste--whole.dark ---black -isp?ajax                                               
+>> drupal .match--ajax .pro-kvr-dataabase _let.go:live --torrent.pad ---tap.bpm                                         
+>> smuch ..ttv--kvr -audio.taste ---waldorf.sony ---grow.acrym--digitalis.drug :match.power..rat.poison                 
+>> AFI:match---specnaz ..trotter-delete .frog--shark _enemie.dust --iman .trup ---rupert.power _film                    
+>> 'run
+ParserError:
+Line |
+   5 |  'run
+     |   ~~~
+     | Unexpected token 'run' in expression or statement.
+
+PS C:\Users\Pedroxian>  
