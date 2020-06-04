732a2fe156bf79815eb9f1ea16bad25620898b42
pedroxian <44950875+pedroxian@users.noreply.github.com>
2020-06-04 22:29:15 +0200

    new bios
    attend matrix

---
+++ batch-conform.bat
@@ -0,0 +1,17 @@
+boot.avs /conform .dude _/let.mage if.self
+    root.batch.operation---black .frog /unimog
+    
+        active.confuse -batch.dos/free.dos
+            ibm.act/asus --dell.acer hp.omen
+            
+                factory.up  --tuner.speed..dos
+                
+                    boot.efi/tuner.athlon -to/ryzen
+                        for.i5_else.i3
+                            ease.for (test) {
+                                mute.dos /aprox..leave fl.studio
+                                    ls.script --lightwave/z.brush
+                                    
+                                        cage.9/7.0 --else.shark -whole.blueprints/blackwidow
+                            }
+pause
